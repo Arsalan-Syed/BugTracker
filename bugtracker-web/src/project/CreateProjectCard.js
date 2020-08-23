e49ca67b1d4a3f3031914da3bfff83eceb021589
Arsalan-Syed <arsalan.s96@gmail.com>
2020-08-23 22:55:00 +0100

    Convert project cards from class to stateless function

--- bugtracker-web/src/project/CreateProjectCard.js
+++ bugtracker-web/src/project/CreateProjectCard.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './CreateProjectCard.css'
 
-class CreateProjectCard extends Component{
 
-    render() {
-        return <div className="CreateProjectCard">
-            <b className="CreateProjectCardTitle">Create a project</b>
-        </div>;
-    }
-}
+const CreateProjectCard = () => (
+    <div className="CreateProjectCard">
+        <b className="CreateProjectCardTitle">Create a project</b>
+    </div>
+);
 
 export default CreateProjectCard;
--- bugtracker-web/src/project/ProjectCard.js
+++ bugtracker-web/src/project/ProjectCard.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './ProjectCard.css'
 
-class ProjectCard extends Component{
 
-    render() {
-        return <div className="ProjectCard" style={{"background-color": this.props.project.color}}>
-            <b className="ProjectCardTitle">{this.props.project.name}</b>
-        </div>;
-    }
-}
+const ProjectCard = props => (
+    <div className="ProjectCard" style={{"background-color": this.props.project.color}}>
+        <b className="ProjectCardTitle">{this.props.project.name}</b>
+    </div>
+);
 
 export default ProjectCard;
