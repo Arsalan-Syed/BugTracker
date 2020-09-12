98860656bd6e47d6b0f5190ecd182226f4e15a43
Arsalan-Syed <arsalan.s96@gmail.com>
2020-09-12 19:50:20 +0100

    Modal for creating project

--- bugtracker-web/src/Pages/ProjectsPage.js
+++ bugtracker-web/src/Pages/ProjectsPage.js
@@ -1,16 +1,50 @@
 import React, {Component} from 'react';
 import ProjectCard from "../Components/ProjectCard";
+import Modal from "react-bootstrap/Modal";
 
 
 export default class ProjectsPage extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            modalOpen: false
+        };
+    }
+
+    showModal = () => {
+        this.setState({
+            modalOpen: true
+        })
+    }
+
+    hideModal = () => {
+        this.setState({
+            modalOpen: false
+        })
+    }
+
     render(){
         return (
             <div className="container-fluid">
+                <Modal show={this.state.modalOpen}>
+                    <Modal.Header>
+                        <Modal.Title>
+                            Create an new project
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Body</Modal.Body>
+                    <Modal.Footer>
+                        <button className="btn btn-danger" onClick={this.hideModal}>Cancel</button>
+                        <button className="btn btn-primary" onClick={this.hideModal}>Save</button>
+                    </Modal.Footer>
+                </Modal>
+
                 <div className="d-sm-flex align-items-center justify-content-between mb-4">
                     <h1 className="h3 mb-0 text-gray-800">Projects</h1>
-                    <a href="#" className="btn-primary btn-circle">
+                    <button className="btn-primary btn-circle" onClick={this.showModal}>
                         <i className="fab">+</i>
-                    </a>
+                    </button>
                 </div>
 
                 <div className="row">
